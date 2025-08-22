3a48254339bea022c163c24d20700db8aaf85e81
Panvish Dowripilli <panvishd@gmail.com>
2025-08-23 01:25:40 +0530

    SearchBar Component Done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,8 @@ import {Link} from "expo-router";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
+import {useRouter} from "expo-router";
+import {use} from "react";
 
 // Home Page
 
@@ -26,6 +28,7 @@ import SearchBar from "@/components/SearchBar";
 // In app.json we can change the Icon, Name, ......
 
 export default function Index() {
+    const router = useRouter();
   return (
     <View className="flex-1 bg-primary">
         <Image source={images.bg} className="absolute w-full z-0"/>
@@ -36,7 +39,10 @@ export default function Index() {
             <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
             <View className="flex-1 mt-5">
-                <SearchBar />
+                <SearchBar
+                    onPress={() => router.push("/search")}
+                    placeholder="Search for a movie"
+                />
             </View>
         </ScrollView>
     </View>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,13 +3,18 @@ import React from 'react'
 import {icons} from "@/constants/icons";
 import {images} from "@/constants/images";
 
-const SearchBar = () => {
+interface Props{
+    placeholder: string;
+    onPress?: () => void;
+}
+
+const SearchBar = ({placeholder, onPress}: Props) => {
     return (
         <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
             <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
             <TextInput
-                onPress={() => {}}
-                placeholder="Search"
+                onPress={onPress}
+                placeholder={placeholder}
                 value=""
                 onChange={() => {}}
                 placeholderTextColor="#a8b5db"
