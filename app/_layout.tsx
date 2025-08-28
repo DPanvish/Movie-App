4ae3a0fa243fe0bacb1ea57d314eeecf118909f6
Panvish Dowripilli <panvishd@gmail.com>
2025-08-29 01:02:51 +0530

    Hidden Status Bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,28 @@
 import { Stack } from "expo-router";
 import "./globals.css"
+import {StatusBar} from "react-native";
 
 // This file is for the layout of the whole app
 
 export default function RootLayout() {
-  return <Stack>
+  return(
+      <>
+          {/* This is to hide the status bar in the whole app*/}
+          <StatusBar hidden={true}/>
+          <Stack>
 
-    {/* This is to hides the route groups header (tabs) in the (tabs)*/}
-    <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: false }}
-    />
+              {/* This is to hides the route groups header (tabs) in the (tabs)*/}
+              <Stack.Screen
+                  name="(tabs)"
+                  options={{ headerShown: false }}
+              />
 
-    {/* This is to hide the header in the movie details page*/}
-    <Stack.Screen
-      name="movie/[id]"
-      options={{ headerShown: false }}
-    />
-  </Stack>;
+              {/* This is to hide the header in the movie details page*/}
+              <Stack.Screen
+                  name="movie/[id]"
+                  options={{ headerShown: false }}
+              />
+          </Stack>
+      </>
+    );
 }
