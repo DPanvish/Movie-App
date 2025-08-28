67659d08a613a1b5551c4ea21399a24bf0c8e299
Panvish Dowripilli <panvishd@gmail.com>
2025-08-29 01:51:06 +0530

    Saved Page fixed

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -6,7 +6,7 @@ const Profile = () => {
     return (
         <View className="bg-primary flex-1 px-10">
             <View className="flex justify-center items-center flex-1 flex-col gap-5">
-                <Image source={icons.person} className="size-20" tintColor="#fff" />
+                <Image source={icons.person} className="size-10" tintColor="#fff" />
                 <Text className="text-gray-500 text-base">Profile</Text>
             </View>
         </View>
