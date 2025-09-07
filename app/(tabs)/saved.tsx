40528d436c90e901f7e1e4522ef2798b1595ab70
Panvish Dowripilli <panvishd@gmail.com>
2025-09-08 00:42:42 +0530

    Saved Tab

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,8 +1,28 @@
 import {Image, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useCallback} from 'react';
 import {icons} from "@/constants/icons";
+import {useRouter} from "expo-router";
+import useFetch from "@/services/useFetch";
+import {deleteSavedMovie, listSavedMovies} from "@/services/appwrite";
 
 const Saved = () => {
+    const router = useRouter();
+
+    // To get the saved movies from the database
+    const {data: savedMovies, loading, error, refetch} = useFetch(listSavedMovies);
+
+    const onOpenMovie = useCallback((movieId: number) => {
+        router.push(`/movies/${movieId}` as any);
+    }, [router]);
+
+    const onDelete = useCallback(async (movieId: number) => {
+        try{
+            await deleteSavedMovie(movieId);
+            await refetch();
+        }catch(error){
+            console.log("Error deleting movie: ", error);
+        }
+    }, [refetch])
     return (
         <View className="bg-primary flex-1 px-10">
             <View className="flex justify-center items-center flex-1 flex-col gap-5">
